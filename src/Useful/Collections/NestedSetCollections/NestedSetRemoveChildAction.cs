2031d18564c1506c84a10bf016bf9d03d5043f45
Cecer <Cecer1@cecer1.com>
2012-08-22 23:59:50 +0100

    Ported NestedSet collections. Ported BiDirectionalDictionary collection.

---
+++ src/Useful/Collections/NestedSetCollections/NestedSetRemoveChildAction.cs
@@ -0,0 +1,10 @@
+﻿namespace Bluedot.HabboServer.Useful.Collections
+{
+    public enum NestedSetRemoveChildAction
+    {
+        RecursiveDelete,
+        MoveUpGeneration,
+        ThrowException,
+        ReturnFalse
+    }
+}
